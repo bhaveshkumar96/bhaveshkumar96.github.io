a18d0540bf51ccb30f966b04cc6029e1ea55c11a
BhaveshKumar <bk769221@gmail.com>
2023-02-28 15:38:35 +0530

    new project added

--- src/Routes/Projects.jsx
+++ src/Routes/Projects.jsx
@@ -17,6 +17,95 @@ export const Projects = () => {
     <div name="main_projects_link" className={styles.mainProjects} style={{backgroundImage:"url('https://images.pexels.com/photos/1072179/pexels-photo-1072179.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')"}}>
       <EveryHeading style={{color:"whitesmoke",marginTop:"20px",marginBottom:"0px" }}>PROJECTS</EveryHeading>
       <div className={styles.mainProjects_mainDiv}>
+{/* caratlane project */}
+<div className={styles.mainProjects_mainDiv_project1}>
+          <div className={styles.mainProjects_mainDiv_project1_img}>
+            <img src={"https://user-images.githubusercontent.com/108007506/221486611-cf7cb23e-912a-40c2-8f32-941c7bc0cf2b.png"} alt="caratlane.com" />
+          </div>
+          <div className={styles.mainProjects_mainDiv_project1_summary}>
+            <div>
+              {" "}
+              <h2>Caratlane.com</h2>
+              <p>
+              Caratlane is a Tanishq partner jewelry ecommerce website - clone .It was a Collaborative Project built in 5 days by 4 team members, App was built using React-redux ,HTML,CSS,JavaScript,and Chakra-ui and for backend we have used MongoDB,Mongoose,and Cyclic.
+              </p>
+            </div>
+            <div className={styles.mainProjects_mainDiv_project1_summary_flex}>
+              <div>
+                <a target="_blank" href="https://24karat.netlify.app/">
+                  <button className={styles1.viewDemoButton}>Live</button>
+                </a>
+              </div>
+              <div>
+                <a
+                  target="_blank"
+                  href="https://github.com/mrprincesingh/freezing-value-6715"
+                >
+                  <button className={styles1.viewDemoButton}>
+                    View Source
+                  </button>
+                </a>
+              </div>
+              <div
+                className={
+                  styles.mainProjects_mainDiv_project1_summary_flex_icon
+                }
+              >
+                <Icon_small className="devicon-react-original-wordmark colored"></Icon_small>
+                <Icon_small className="devicon-css3-plain-wordmark colored"></Icon_small>
+                <Icon_small className="devicon-javascript-plain colored"></Icon_small>
+                <Icon_small className="devicon-html5-plain-wordmark colored"></Icon_small>
+                
+              </div>
+            </div>
+            
+          </div>
+        </div>
+{/* SNAPDEAL PROJECT STARTS HERE */}
+      <div className={styles.mainProjects_mainDiv_project1}>
+          <div className={styles.mainProjects_mainDiv_project1_img}>
+            <img src={baby} alt="shop.com" />
+          </div>
+          <div className={styles.mainProjects_mainDiv_project1_summary}>
+            <div>
+              {" "}
+              <h2>Snapdeal.com</h2>
+              <p>
+              Snapdeal is an Indian e-commerce website Snapdeal was one of the largest online marketplaces in India. Snapdeal targets the value e-commerce segment.It was a Collaborative Project built in 5 days, App was built using React-redux ,HTML,CSS,JavaScript,and Chakra-ui.
+              </p>
+            </div>
+            <div className={styles.mainProjects_mainDiv_project1_summary_flex}>
+              <div>
+                <a target="_blank" href="https://snapdeal-project.vercel.app/">
+                  <button className={styles1.viewDemoButton}>Live</button>
+                </a>
+              </div>
+              <div>
+                <a
+                  target="_blank"
+                  href="https://github.com/bhaveshkumar96/brash-profit-9147"
+                >
+                  <button className={styles1.viewDemoButton}>
+                    View Source
+                  </button>
+                </a>
+              </div>
+              <div
+                className={
+                  styles.mainProjects_mainDiv_project1_summary_flex_icon
+                }
+              >
+                <Icon_small className="devicon-react-original-wordmark colored"></Icon_small>
+                <Icon_small className="devicon-css3-plain-wordmark colored"></Icon_small>
+                <Icon_small className="devicon-javascript-plain colored"></Icon_small>
+                <Icon_small className="devicon-html5-plain-wordmark colored"></Icon_small>
+                
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        
         <div className={styles.mainProjects_mainDiv_project1}>
           <div className={styles.mainProjects_mainDiv_project1_img}>
             <img src={purplle} alt="" />
@@ -82,7 +171,7 @@ export const Projects = () => {
               <div>
                 <a
                   target="_blank"
-                  href="https://github.com/agrawalrajat310/fertile-distance-9246"
+                  href="https://spectacular-otter-c3b514.netlify.app/"
                 >
                   <button className={styles1.viewDemoButton}>Live</button>
                 </a>
@@ -90,7 +179,7 @@ export const Projects = () => {
               <div>
                 <a
                   target="_blank"
-                  href="https://github.com/jayprakashlohar/hardy-mouth-2466/tree/main/licious.in"
+                  href="https://github.com/agrawalrajat310/DNA-News-clone"
                 >
                   <button className={styles1.viewDemoButton}>
                     View Source
@@ -154,49 +243,7 @@ export const Projects = () => {
             
           </div>
         </div>
-        <div className={styles.mainProjects_mainDiv_project1}>
-          <div className={styles.mainProjects_mainDiv_project1_img}>
-            <img src={baby} alt="shop.com" />
-          </div>
-          <div className={styles.mainProjects_mainDiv_project1_summary}>
-            <div>
-              {" "}
-              <h2>Snapdeal.com</h2>
-              <p>
-              Snapdeal is an Indian e-commerce website Snapdeal was one of the largest online marketplaces in India. Snapdeal targets the value e-commerce segment.It was a Collaborative Project built in 5 days, App was built using React-redux ,HTML,CSS,JavaScript,and Chakra-ui.
-              </p>
-            </div>
-            <div className={styles.mainProjects_mainDiv_project1_summary_flex}>
-              <div>
-                <a target="_blank" href="https://snapdeal-project.vercel.app/">
-                  <button className={styles1.viewDemoButton}>Live</button>
-                </a>
-              </div>
-              <div>
-                <a
-                  target="_blank"
-                  href="https://github.com/bhaveshkumar96/brash-profit-9147"
-                >
-                  <button className={styles1.viewDemoButton}>
-                    View Source
-                  </button>
-                </a>
-              </div>
-              <div
-                className={
-                  styles.mainProjects_mainDiv_project1_summary_flex_icon
-                }
-              >
-                <Icon_small className="devicon-react-original-wordmark colored"></Icon_small>
-                <Icon_small className="devicon-css3-plain-wordmark colored"></Icon_small>
-                <Icon_small className="devicon-javascript-plain colored"></Icon_small>
-                <Icon_small className="devicon-html5-plain-wordmark colored"></Icon_small>
-                
-              </div>
-            </div>
-            
-          </div>
-        </div>
+        
       </div>
     </div>
   );
